8ba34633a442d7f02742a8c9c5ce0d8bbc504683
Ibrahim <i-m-6@hotmail.com>
2023-07-07 19:23:59 +0300

    Initial Commit

---
+++ SwimSwiftUi/SwimSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwimSwiftUiApp.swift
+//  SwimSwiftUi
+//
+//  Created by ibrahim asiri on 19/12/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct SwimSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
