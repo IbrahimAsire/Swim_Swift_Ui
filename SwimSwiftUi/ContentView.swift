ba0b7105f28ebe35ada4ddcf61a02cc00a932bd7
Ibrahim <i-m-6@hotmail.com>
2023-07-13 18:23:30 +0300

    format: some words

--- SwimSwiftUi/ContentView.swift
+++ SwimSwiftUi/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State var tapCount = 0
     @State var name = ""
-    
+
     var body: some View {
         NavigationView {
             Form {
@@ -27,7 +27,7 @@ struct ContentView: View {
                     
                 }
                 
-                Button("Tapped Here: \(tapCount)"){
+                Button("Tapped Here: \(tapCount) this's you number"){
                     self.tapCount += 1
                 }
                 .buttonStyle(.bordered).font(.headline)
