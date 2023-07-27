cf3242ab960d67a90f903925745cc43b733a6379
Ibrahim <i-m-6@hotmail.com>
2023-07-27 11:33:33 +0300

    feat: enhancement of the interface

--- SwimSwiftUi/ContentView.swift
+++ SwimSwiftUi/ContentView.swift
@@ -55,6 +55,8 @@ struct ContentView: View {
                 }
                 
                 Section {
+                    Text("How much tip do you want to leave?")
+                    
                     Picker("Tip percentage", selection: $tipPercentage) {
                         ForEach(tipPercentages, id: \.self) {
                             Text($0, format: .percent)
